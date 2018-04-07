384a155460fc896469c2c03fafa09e4097670eb6
wegar-2 <prometheus0209@gmail.com>
2018-04-07 20:56:22 +0200

    Making script work to test-run  it.

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ import os
 import sys
 import logging
 # Python looks for imports in the list of paths sys.path
-import download_data
+import download_data as dd
 
 
 ### 0. prepare a logger
-mlogger = logging.getLogger(name="main_logger") # create a logger
-mlogger.setLevel(level=logging.INFO) # set the level on the mlogger logger
-# create a handler for mlogger
-ch = logging.StreamHandler() # create a handler
-ch.setLevel(level=logging.INFO) # set the level for the handler
-# configure the formatting of the output produced by console handler
-formatter = logging.Formatter(fmt="%(asctime)s - %(name)s - %(level)s - %(message)s")
-ch.setFormatter(fmt=formatter)
-# add the console handler to mlogger
-mlogger.addHandler(hdlr=ch)
+# mlogger = logging.getLogger(name="main_logger") # create a logger
+# mlogger.setLevel(level=logging.INFO) # set the level on the mlogger logger
+# # create a handler for mlogger
+# ch = logging.StreamHandler() # create a handler
+# ch.setLevel(level=logging.INFO) # set the level for the handler
+# # configure the formatting of the output produced by console handler
+# formatter = logging.Formatter(fmt="%(asctime)s - %(name)s - %(level)s - %(message)s")
+# ch.setFormatter(fmt=formatter)
+# # add the console handler to mlogger
+# mlogger.addHandler(hdlr=ch)
 
 
 # 1. Prepare some other useful variables
-mlogger.info(msg="Setting up variables and configuring the directories. ")
+# mlogger.info(msg="Setting up variables and configuring the directories. ")
 
 this_script_path = os.path.abspath(__file__)
 this_script_dir = os.path.dirname(this_script_path) # package directory
@@ -32,5 +32,10 @@ if not os.path.isfile(path=data_dir):
 
 if __name__ == "__main__":
     # call the script that downloads the data from http://powietrze.gios.gov.pl/pjp/archives and unpacks it
-    mlogger.info(msg="Looking for data on disk and/or getting the data from GIOS website.")
+    # mlogger.info(msg="Looking for data on disk and/or getting the data from GIOS website.")
     os.system("python3.6 download_data.py")
+    # calling the data-downloading function
+    print("calling the data-downloading function")
+    dd.download_data(yearly_datasets_dict=dd.yearly_datasets_dict,
+                     giodo_pjp_url=dd.giodo_pjp_url,
+                     data_dir=data_dir)
