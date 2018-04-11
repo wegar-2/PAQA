5ed4a4abad06d3c217ba50f6a9da56202fe02960
wegar-2 <prometheus0209@gmail.com>
2018-04-11 21:14:29 +0200

    Adding the main.py script - its updated version.

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import os
-import sys
 import logging
 import download_data as dd
 
 # ----------------------------------------------------------------------------------------------------------------------
 # 1. Set up logging
 # create a logger and set its severity
-mlogger = logging.getLogger(name="my_logger")
-mlogger.setLevel(level=logging.INFO)
+main_logger = logging.getLogger(name="main_logger")
+main_logger.setLevel(level=logging.INFO)
 
 # create a handler and set its severity
 ch = logging.StreamHandler()
@@ -18,43 +16,22 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 
 # add the handler to the logger
-mlogger.addHandler(ch)
+main_logger.addHandler(hdlr=ch)
 # ----------------------------------------------------------------------------------------------------------------------
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-# 2. Prepare some other useful variables
-# get directory to the folder with this script
-this_script_dir = os.path.dirname(os.path.abspath(__file__)) # package directory
-# check whether a folder /data exists
-data_dir = os.path.join(this_script_dir, "data")
-if not os.path.exists(path=data_dir):
-    mlogger.info("Folder for data does not exist yet - creating a new one. ")
-    os.system(command="mkdir " + data_dir)
-else:
-    mlogger.info("Folder for data already exists.")
-# ----------------------------------------------------------------------------------------------------------------------
-
-
-# ----------------------------------------------------------------------------------------------------------------------
-# 3. Setting up the data base
+# 2. Setting up the data base
 if __name__ == "__main__":
     # 1. calling the data-downloading function
-    mlogger.info("---------------------------------------------------")
-    mlogger.info("1. ")
-    mlogger.info("Checking the contents of /data.")
-    mlogger.info("The /data folder contains the following files: ")
-    for iter_num, iter_el in enumerate(os.listdir(data_dir)):
-        mlogger.info(msg="\t" + str(iter_num) + ": " + iter_el)
-    bool_d = input("Do you want to dowload/re-download files from GIOS archive? "
-                   "Type in y/Y for yes or any other input for no. ")
-    if bool_d in ("y", "Y"):
-        dd.download_data(data_dir=data_dir)
-    else:
-        pass
+    main_logger.info("---------------------------------------------------")
+    main_logger.info("1. Downloading and/or uploaging data from GIOS website. ")
+    main_logger.info("Calling the download_data function...")
+    dd.download_data()
+
     # 2. logging in as root to MySQL DB server to set up a schema and a user
-    mlogger.info("---------------------------------------------------")
-    mlogger.info("2. ")
-    mlogger.info("Uploading the data into MySQL server. ")
+    main_logger.info("---------------------------------------------------")
+    main_logger.info("2. ")
+    main_logger.info("Uploading the data into MySQL server. ")
 
 
