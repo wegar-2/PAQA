c4716144783c64f3524f8a4acc87c286de3e8905
wegar-2 <prometheus0209@gmail.com>
2018-04-21 18:03:53 +0200

    Update of the scripts that prepare the database.

--- prepare_DB_server.py
+++ prepare_DB_server.py
@@ -4,24 +4,47 @@ import logging
 # 0. getting a logger
 main_logger = logging.getLogger(name="main_logger")
 
-# 1. create connection string
-root_pass = input("Type in the root password. ")
-con_string = "mysql://root:" + root_pass + "@localhost/"
 
-# 2. create connection engine
-mysqldb_engine = sa.create_engine(con_string)
+def prepare_DB_server():
 
-# 3. create schema, user and grant privileges
-temp_connection = mysqldb_engine.connect()
-temp_connection.execute("CREATE DATABASE IF NOT EXISTS PAQA_DB;")
-temp_connection.execute("CREATE USER IF NOT EXISTS 'paqa_user'@'localhost' as identified by 'pass';")
-temp_connection.execute("GRANT ALL ON PAQA_DB.* TO 'PAQA_USER'@'LOCALHOST';")
-temp_connection.execute("FLUSH PRIVILEGES; ")
-temp_connection.close()
-del mysqldb_engine
+    # 1. create connection string
+    main_logger.info("Requesting user password to create target user and schema. ")
+    root_pass = input("Type in the root password. ")
+    con_string = "mysql://root:" + root_pass + "@localhost/"
 
-# 4. create connection engine as PAQA_USER to PAQA_DB
-mysqldb_engine = sa.create_engine("mysql://PAQA_USER:pass@localhost/PAQA_DB")
+    # 2. create connection engine
+    mysqldb_engine = sa.create_engine(con_string)
+
+    # 3. create schema, user and grant privileges
+    try:
+        temp_connection = mysqldb_engine.connect()
+        temp_connection.execute("CREATE DATABASE IF NOT EXISTS PAQA_DB;")
+        temp_connection.execute("CREATE USER IF NOT EXISTS 'PAQA_USER'@'localhost' identified by 'pass';")
+        temp_connection.execute("GRANT ALL ON PAQA_DB.* TO 'PAQA_USER'@'localhost';")
+        temp_connection.execute("FLUSH PRIVILEGES; ")
+        temp_connection.close()
+    except Exception as exc:
+        main_logger.error(msg="Error occurred when creating the schema and user to work with. ")
+        main_logger.error(msg=exc)
+    else:
+        del mysqldb_engine
+
+    # 4. create & test connection engine as PAQA_USER to PAQA_DB
+    try:
+        mysqldb_engine = sa.create_engine("mysql+mysqldb://PAQA_USER:pass@localhost/PAQA_DB")
+    except Exception as exc:
+        main_logger.error(msg="Error occurred when createing a test connection engine...")
+        main_logger.error(msg=exc)
+    else:
+        try:
+            connection1 = mysqldb_engine.connect()
+        except Exception as exc:
+            main_logger.error("An error occurred when testing DB connectivity to the PAQA_DB schema...")
+            main_logger.error(msg=exc)
+        else:
+            connection1.close()
+            main_logger.info(msg="Test connection to DB schema PAQA_DB created and closed successfully!")
+            del connection1, mysqldb_engine
 
 
 
--- setup_of_the_DB.py
+++ setup_of_the_DB.py
@@ -9,6 +9,59 @@ import os
 main_logger = logging.getLogger(name="main_logger")
 
 
-# ----------------------------------------------------------------------------------------------------------------------
-# List of tables to create in the DB:
-#
+def setup_of_the_DB():
+    # ------------------------------------------------------------------------------------------------------------
+    # List of tables to create in the DB:
+    # 1. CITIES
+    # 2. STATIONS
+    # 3. POLLUTION_DATA
+    main_logger.info("Inside function setup_of_the_DB. Starting creation of tables. ")
+    main_logger.info("\n\nThe following tables will be created: ")
+    main_logger.info("\n\t1. CITIES\n\t2. STATIONS\n\t3. POLLUTION_DATA\n\n\n")
+
+    mysqldb_engine = sa.create_engine("mysql+mysqldb://PAQA_USER:pass@localhost/PAQA_DB")
+
+    # ------------------------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------
+    # 1. Creating table: CITIES
+    main_logger.info("\n\nCreating table: CITIES")
+    metadata1 = sa.MetaData(bind=mysqldb_engine)
+    try:
+        sa.Table('CITIES', metadata1,
+                 sa.Column('id_city', sa.INT, primary_key=True,
+                           autoincrement=True, nullable=False),
+                 sa.Column('city_name', sa.VARCHAR(100), nullable=False))
+        metadata1.create_all()
+    except Exception as exc:
+        main_logger.error(msg="Error occurred when trying to create table CITIES")
+        main_logger.error(msg=exc)
+
+    # ------------------------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------
+    # 2. Creating table: STATIONS
+    main_logger.info("\n\nCreating table: STATIONS")
+    try:
+        sa.Table('STATIONS', metadata1,
+                 sa.Column('id_station', sa.INT, primary_key=True, autoincrement=True, nullable=False),
+                 sa.Column('station_name', sa.VARCHAR(100), nullable=False),
+                 sa.Column('id_city', sa.INT, sa.ForeignKey('CITIES.id_city'), nullable=False))
+        metadata1.create_all()
+    except Exception as exc:
+        main_logger.error(msg="Error occurred when trying to create table STATIONS")
+        main_logger.error(msg=exc)
+
+    # ------------------------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------
+    main_logger.info("\n\nCreating table: POLLUTION_DATA")
+    try:
+        sa.Table('POLLUTION_DATA', metadata1,
+                 sa.Column('id_pollution_data', sa.INT, primary_key=True, autoincrement=True, nullable=False),
+                 sa.Column('id_station', sa.INT, sa.ForeignKey('STATIONS.id_station'), nullable=False),
+                 sa.Column('measurement_date', sa.DATE, nullable=False),
+                 sa.Column('measurement_value', sa.FLOAT(18, 4), nullable=False),
+                 sa.Column('measurement_unit', sa.VARCHAR(10), nullable=False))
+        metadata1.create_all()
+    except Exception as exc:
+        main_logger.error(msg="Error occurred when trying to create table POLLUTION_DATA")
+        main_logger.error(msg=exc)
+
